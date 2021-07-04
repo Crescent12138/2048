563eb07110a81f272340a5ed4a15988cc9dd257b
Crescent12138 <75103155+Crescent12138@users.noreply.github.com>
2021-07-04 19:41:13 +0800

    Add files via upload

---
+++ 2048/2048/2048/2048.cpp
@@ -0,0 +1,111 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<string.h>
+#include<stdbool.h>
+#include<assert.h>
+#include<math.h>
+#include<Windows.h>
+#include<stddef.h>
+#include <corecrt_malloc.h>
+#include"move.h"
+#include"Ranking_System.h"
+//移动函数装里面了，为了体现代码不是cv的，所以做了一点小小的防护
+// 
+// ----------------------------------------------------------------------
+// 
+//  ————        /\         ————      ———  -------    -       
+// |               /  \        |       |       |     |          |       
+// |              /    \       |       |       |     |          |       
+//  ————     /______\      |————       |     |------    |       
+//          |   /        \     |        \      |     |          |       
+//          |  /          \    |         \     |     |          |       
+//  ————  /            \   |          \  ———  --------   ---------  
+// 
+// ----------------------------------------------------------------------
+// 
+// 以上为我大概认为用的到的头文件，包括常用宏定义，字符串，输出入，布尔判定，时间，算法随机，断言，数学,自制移动函数
+
+//这里是菜单
+void menu() {
+    printf("here is 2048.\n");
+    system("pause");
+}
+
+int wan_jia[4][4] = {};
+
+//判定是否可移动，若不可则继续读取数字
+bool move_pan_ding( char step ) {
+    int tmp[4][4] = {};
+    bool ok = 0;
+    for (int i = 0; i < 4; i++) {
+        
+        for (int j = 0; j < 4; j++) {
+            
+            tmp[i][j] = wan_jia[i][j];
+
+        }
+    }
+
+    switch (step){
+    case 'w': 
+        up(tmp)
+        
+        
+        
+        ; break;
+
+    case 's': 
+        down(tmp)
+        
+        
+        ; break;
+
+    case 'a': 
+        
+        
+        
+        
+        ; break;
+
+    case 'd': 
+        
+        
+        
+        ; 
+        
+        break;
+
+    }
+    return 1;
+}
+
+void print(int shu_zu[4][4]) {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("%d\t",shu_zu[i][j]);
+        }
+        printf("\n");
+    }
+}
+void test() {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            wan_jia[i][j] = 2;
+        }
+    }
+    down(wan_jia);
+    print(wan_jia);
+    left(wan_jia);
+    print(wan_jia);
+    right(wan_jia);
+    print(wan_jia);
+
+}
+int main() {
+   // menu();
+   
+    Print_Leaderboard();
+    
+
+}
---
+++ 2048/2048/2048/Ranking_System.h
@@ -0,0 +1,97 @@
+#pragma once
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<Windows.h>
+#include<stdlib.h>
+#include<conio.h>
+
+struct Completion_time {//游戏成就达成时间
+	int year;
+	int month;
+	int day;
+};
+
+struct Leaderboard {                                             //排行榜中的元素内容
+	char name[100];                                              //游戏用户名 && 只允许英文字符
+	int game_Step;
+	Completion_time completion_time;
+}leaderboard[10];
+int Sort_rules(const void* a, const void* b)//排序规则
+{
+	Leaderboard* Sort_Elements_One = (Leaderboard*)a;
+	Leaderboard* Sort_Elements_Two = (Leaderboard*)b;
+	if ((*Sort_Elements_One).game_Step != (*Sort_Elements_Two).game_Step)								//关键字1：游戏步数
+		return (*Sort_Elements_One).game_Step > (*Sort_Elements_Two).game_Step ? 1 : -1;
+
+	else if ((*Sort_Elements_One).completion_time.year != (*Sort_Elements_Two).completion_time.year)	//关键字2：完成年份
+		return (*Sort_Elements_One).completion_time.year > (*Sort_Elements_Two).completion_time.year ? 1 : -1;
+
+	else if ((*Sort_Elements_One).completion_time.month != (*Sort_Elements_Two).completion_time.month)	//关键字3：完成月份
+		return (*Sort_Elements_One).completion_time.month > (*Sort_Elements_Two).completion_time.month ? 1 : -1;
+
+	else if ((*Sort_Elements_One).completion_time.day != (*Sort_Elements_Two).completion_time.day)		//关键字4：完成日
+		return (*Sort_Elements_One).completion_time.day > (*Sort_Elements_Two).completion_time.day ? 1 : -1;
+
+	else if (strlen((*Sort_Elements_One).name) != strlen((*Sort_Elements_Two).name))			     	//关键字5：名称长度
+		return strlen((*Sort_Elements_One).name) > strlen((*Sort_Elements_Two).name) ? 1 : -1;
+
+	//关键字6：字母ACSII顺序
+	else {
+
+
+		for (int i = 0; i < strlen((*Sort_Elements_One).name); i++) {
+
+			if ((*Sort_Elements_One).name[i] != (*Sort_Elements_Two).name[i])
+				return (*Sort_Elements_One).name[i] < (*Sort_Elements_Two).name[i] ? 1 : -1;
+
+		}
+
+	}
+}
+void Initialization_Leaderboard() {
+	for (int i = 0; i < 10; i++) {
+		leaderboard[i].game_Step = 99999;
+		strcpy(leaderboard[i].name, "empty");
+		leaderboard[i].completion_time.year = leaderboard[i].completion_time.month = leaderboard[i].completion_time.day = 9999;
+	}
+}
+void Print_Leaderboard() {//输出排行榜
+	FILE* fp = NULL;//文件指针->打开排行榜信息
+	fp = fopen("Leaderboard.txt", "r");
+	if (fp == NULL) {//读取本地信息出错
+		printf("There is a bug in reading information, please restart and try.\n"
+
+			"If loading fails all the time, please contact the administrator:\nQQ：2986325137 / 1272607918\n"
+
+			"");
+		getch();//暂停程序
+		system("cls");//清空屏幕缓存区
+		return;
+	}
+	Initialization_Leaderboard();//初始化排行榜结构体
+	int local_Population = 0;//本地保存的人数
+	while ((fscanf(fp, "%s", leaderboard[++local_Population].name)) != EOF) {
+
+		fscanf(fp, "%d", &leaderboard[local_Population].game_Step);
+
+		fscanf(fp, "%d", &leaderboard[local_Population].completion_time.year);
+
+		fscanf(fp, "%d", &leaderboard[local_Population].completion_time.month);
+
+		fscanf(fp, "%d", &leaderboard[local_Population].completion_time.day);
+	}
+	qsort(leaderboard, 10, sizeof leaderboard[0], Sort_rules);//对排名信息进行排序
+
+	printf("GamerName\tGameStep\tAchieveTime\n");
+
+	for (int i = 0; i < 10; i++) {
+
+		printf("%s\t\t%d\t\t%d.%d.%d\n", leaderboard[i].name, leaderboard[i].game_Step, leaderboard[i].completion_time.year, leaderboard[i].completion_time.month, leaderboard[i].completion_time.day);
+
+	}
+
+	printf("Press any key to return to the main menu!\n");
+
+	getch();
+}
