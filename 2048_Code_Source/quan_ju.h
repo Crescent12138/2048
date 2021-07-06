0f684f5f7889d0c43ed1602721f62b9e574009f4
ChinesePeach <79407037+ChinesePeach@users.noreply.github.com>
2021-07-06 16:36:33 +0800

    Add files via upload

---
+++ 2048_Code_Source/quan_ju.h
@@ -0,0 +1,21 @@
+#pragma once
+//c
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<string.h>
+#include<stdbool.h>
+#include<assert.h>
+#include<math.h>
+#include<Windows.h>
+#include<stddef.h>
+#include<conio.h>
+
+//c++
+#include<string>
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+int wan_jia[4][4];
